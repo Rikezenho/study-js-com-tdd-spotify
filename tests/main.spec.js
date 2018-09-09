b95d94ae29b904950762a283cf94341baf430868
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-09-08 22:57:32 -0300

    More lint and before/afterEach hooks

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -9,6 +9,7 @@ import {
   searchTracks,
   searchPlaylists
 } from "../src/main";
+import { isContext } from "vm";
 
 global.fetch = require("node-fetch");
 
@@ -45,25 +46,38 @@ describe("Spotify Wrapper", () => {
   });
 
   describe("Generic Search", () => {
-    it("should call fetch function", () => {
-      const fetchedStub = sinon.stub(global, "fetch");
-      search();
-
-      expect(fetchedStub).to.have.been.calledOnce;
+    let fetchedStub;
+    beforeEach(() => {
+      fetchedStub = sinon.stub(global, "fetch");
+    });
+    afterEach(() => {
       fetchedStub.restore();
     });
-    it("should receive the correct url to fetch", () => {
-      const fetchedStub = sinon.stub(global, "fetch");
-      search("Incubus", "artist");
 
-      expect(fetchedStub).to.have.been.calledWith(
-        "https://api.spotify.com/v1/search?q=Incubus&type=artist"
-      );
+    it("should call fetch function", () => {
+      search();
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+    it("should call fetch with the correct URL", () => {
+      context("passing one type", () => {
+        search("Incubus", "artist");
 
-      search("Incubus", "album");
-      expect(fetchedStub).to.have.been.calledWith(
-        "https://api.spotify.com/v1/search?q=Incubus&type=album"
-      );
+        expect(fetchedStub).to.have.been.calledWith(
+          "https://api.spotify.com/v1/search?q=Incubus&type=artist"
+        );
+
+        search("Incubus", "album");
+        expect(fetchedStub).to.have.been.calledWith(
+          "https://api.spotify.com/v1/search?q=Incubus&type=album"
+        );
+      });
+      context("passing more than one type", () => {
+        search("Incubus", ["artist", "album"]);
+
+        expect(fetchedStub).to.have.been.calledWith(
+          "https://api.spotify.com/v1/search?q=Incubus&type=artist,album"
+        );
+      });
     });
   });
 });
