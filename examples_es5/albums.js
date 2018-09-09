c1bf6040a9bdefdb3e6c1bb866e6c23b484547cf
Henrique Kuwai <eu@henriquekuwai.com.br>
2018-09-09 02:44:31 -0300

    Babel updates + compiled lib + example index

--- examples_es5/albums.js
+++ examples_es5/albums.js
@@ -3,9 +3,7 @@
 var _lib = require("../lib");
 
 global.fetch = require("node-fetch");
-
 var albums = (0, _lib.searchAlbums)("Incubus");
-
 albums.then(function (data) {
   return data.albums && data.albums.items && data.albums.items.map(function (item) {
     return console.log(item.name);
